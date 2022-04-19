a8161a1a8b716c0e4638645b013479821d70ff84
Nidhi <nidhisunil2000@gmail.com>
2022-04-19 19:36:20 -0300

    pages set up and data being rendered for learn

--- server.py
+++ server.py
@@ -7,19 +7,107 @@ from flask import Response, request, jsonify
 import json
 app = Flask(__name__, static_url_path='/static')
 
+
+#DATA
+learnInfo = {
+
+    "1": {
+            "id": "1",
+            "content": "This is the first page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "2": {
+            "id": "2",
+            "content": "This is the second page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "3": {
+            "id": "3",
+            "content": "This is the third page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "4": {
+            "id": "4",
+            "content": "This is the fourth page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "5": {
+            "id": "5",
+            "content": "This is the fifth page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "6": {
+            "id": "6",
+            "content": "This is the sixth page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "7": {
+            "id": "7",
+            "content": "This is the seventh page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "8": {
+            "id": "8",
+            "content": "This is the eighth page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "9": {
+            "id": "9",
+            "content": "This is the ninth page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "10": {
+            "id": "10",
+            "content": "This is the tenth page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+    "11": {
+            "id": "11",
+            "content": "This is the eleventh page content",
+            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+    },
+
+}
+
+
+
 # ROUTES
 
 @app.route('/')
 def welcome():
    return render_template('welcome.html')
 
+@app.route('/middle')
+def middle():
+   return render_template('middle.html')
+
 @app.route('/quiz')
 def quiz():
    return render_template('quiz.html')
 
 @app.route('/learn/<id>')
-def learn():
-   return render_template('learn.html')
+def learn(id=None):
+    global learnInfo;
+    single_data = learnInfo[id]
+    return render_template('learn.html', single_data=single_data)
+
+@app.route('/edit_entry', methods=['GET', 'POST'])
+def edit_entry():
+    global learnTime
+
+
+    json_data = request.get_json()
+    id_rn = json_data["id_rn"]
+    time = json_data["time"]
+
+
+    new_name_entry =  {
+        "id": id_rn,
+        "time": time
+    }
+
+    learnTime[id_rn]=new_name_entry
+    return
 
 if __name__ == '__main__':
    app.run(debug = True)
---
+++ static/learn.js
@@ -0,0 +1,49 @@
+function edit_entry(new_entry,curr_id){
+  let time = new_entry["time"]
+  let data_to_save = {"time": time, "id_rn":curr_id}
+  console.log(data_to_save)
+  $.ajax({
+          type: "POST",
+          url: "edit_entry",
+          dataType : "json",
+          contentType: "application/json; charset=utf-8",
+          data : JSON.stringify(data_to_save),
+          success: function(result){
+              console.log("Success")
+          },
+          error: function(request, status, error){
+              console.log("Error");
+              console.log(request)
+              console.log(status)
+              console.log(error)
+          }
+      });
+}
+
+
+
+
+
+
+$(document).ready(function(){
+
+
+  $(".edit-submit").click(function(){
+
+  /**  let time =  getTime()??**/
+    let curr_id = $('.edit-submit').attr('name')
+    /*let new_entry = {"time": time}*/
+    let new_id = parseInt(curr_id)+1
+    /**If condition for last learn page**/
+    if(new_id == 12){
+      window.location.href = "http://127.0.0.1:5000/middle";
+    }else{
+      window.location.href = "http://127.0.0.1:5000/learn/"+new_id;
+    }
+
+  /**  edit_entry(new_entry, curr_id)**/
+
+
+  })
+
+})
