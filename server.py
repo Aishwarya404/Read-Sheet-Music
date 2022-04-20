414eaf8f9d16a7dbd20eb446c965b39027f94626
Yina <a17858964104@icloud.com>
2022-04-20 17:51:15 -0400

    added learning page text content and images

--- server.py
+++ server.py
@@ -13,58 +13,58 @@ learnInfo = {
 
     "1": {
             "id": "1",
-            "content": "This is the first page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "Music notation is written on a staff. A staff is basically comprised of four spaces and five lines. This is what it looks like:",
+            "image": "http://bp3.blogger.com/_Tad4UOfdqXs/RZTwOqb7whI/AAAAAAAAAAc/A31QI-ZldAM/w1200-h630-p-k-no-nu/staff-2.jpg"
     },
     "2": {
             "id": "2",
-            "content": "This is the second page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "On every staff, there will be a symbol called a clef. There are two types of clefs: a treble clef and a bass clef. The treble clef is used for the higher sounding notes, usually played with the right hand. The bass clef, is used for the lower sounding notes, usually played with the left hand.",
+            "image": "https://www.nicepng.com/png/detail/327-3271386_the-bass-clef-musical.png"
     },
     "3": {
             "id": "3",
-            "content": "This is the third page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "In this module, we will only learn notes on the treble clef. Here’s an overview of that will look like:",
+            "image": "https://www.dummies.com/wp-content/uploads/212454.image2.jpg"
     },
     "4": {
             "id": "4",
-            "content": "This is the fourth page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "We’ll only be learning one scale, that is, just 8 notes. Let’s start with space notes. F,A,C,E. They spell the word Face!",
+            "image": "https://lessonsinyourhome.net/wp-content/uploads/FACE.jpg"
     },
     "5": {
             "id": "5",
-            "content": "This is the fifth page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "Next, let’s learn line notes. Try to memorize them! If you’re finding this difficult, there’s a hint on the next page",
+            "image": "https://www.musicreadingsavant.com/wp-content/uploads/2011/07/treble-clef-line-notes.jpg?ezimgfmt=rs:282x113/rscb1/ng:webp/ngcb1"
     },
     "6": {
             "id": "6",
-            "content": "This is the sixth page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "The first letter of every word of the following sentence corresponds to this line: 'Every Good Burrito Doesn’t Fall' You can use this to memorize line notes.",
+            "image": "https://www.dacapoalcoda.com/_dl/images/note-positions-tregle-clef-egbdf.png"
     },
     "7": {
             "id": "7",
-            "content": "This is the seventh page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "We just finished learning a scale! Now, let’s move on to beats. The following music notation denotes that you can hold a note down for a maximum of 4 beats in this melody",
+            "image": "https://press.rebus.community/app/uploads/sites/81/2017/07/4-4-time-signature_0001-e1552559771468.png"
     },
     "8": {
             "id": "8",
-            "content": "This is the eighth page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "But how do we know exactly how many beats to hold a note for? If you see a note without any vertical lines on it, then you hold it down for exactly 4 beats. This is also called a whole note.",
+            "image": "https://study.com/cimages/multimages/16/773523164_whole_note.png"
     },
     "9": {
             "id": "9",
-            "content": "This is the ninth page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "If you see a note that is white on the inside and has a vertical line attached to it, then you hold it down for exactly 2 beats. This is also called a half note.",
+            "image": "https://media.istockphoto.com/vectors/black-music-half-note-on-staff-lines-vector-id1186530961?b=1&k=20&m=1186530961&s=170667a&w=0&h=3ly1SosFmcOheh8TtOPQmBqVmzbsyb4uol8XyFTT70w="
     },
     "10": {
             "id": "10",
-            "content": "This is the tenth page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "If you see a note that is coloured black and has a vertical line attached to it, then you hold it down for exactly 1 beat. This is also called a quarter note.",
+            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC_YLoC6DhohF5wQNJVCayTLL2ok9obgp3j9Iqrd0T4wi9GyiK"
     },
     "11": {
             "id": "11",
-            "content": "This is the eleventh page content",
-            "image": "https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8093ba7b5e84ed05/61d8a63ddea73a236fc56d12/Neon_KeyArt-Web.png"
+            "content": "And finally, if you see a note with a vertical line and a squiggly line on it, then you hold it down for exactly half a beat. This is also called an one eighth note.",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/2/23/Eighth_Note_1_%28PSF%29.png"
     },
 
 }
