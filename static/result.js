d8bd869f7face8553a8c5f85800f20fb8722ff88
Aishwarya Sivakumar <aishwaryasivakumar@Aishwaryas-MacBook-Pro.local>
2022-04-28 17:48:59 -0400

    Result page UI and quiz colors

--- static/result.js
+++ static/result.js
@@ -3,23 +3,19 @@ function show_answers() {
         console.log(ind)
         console.log(question_data[ind])
         if (right_ans[ind-1]){
-            var $div =  $("<div> Question " + ind + ". <img src = '/static/pictures/check.png' class='qImage'> </div>")
-            console.log("R")
+            var $div =  $("<div class='right-wrong'> Question " + ind + " <i class='fa fa-check'></i> </div>")
         }
         else{
-            var $div =  $("<div> Question " + ind + ". <img src = '/static/pictures/cross.png' class='qImage'> </div>")
-            console.log("W")
+            var $div =  $("<div class='right-wrong'> Question " + ind + " <i class='fa fa-close'></i> </div>")
         }
-
         $(".answer").append($div)
-        $(".answer").append("<br>")
-    }
-        
+    }    
 }
+
 function show_review_block() {
     let $div = $(`<div> Click on Review to revisit lessons </div>`);
     $("#result-content").append($div)
-    let $link = $(`<div class="link"> <a href="/"> REVIEW </a> </div>`);
+    let $link = $(`<div class="links"> <a href="/"> REVIEW </a> </div>`);
     $("#result-content").append($link)
 }
 
