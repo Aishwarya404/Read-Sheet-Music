dab9ce674b80812aab4b3b38700cab0878f5c356
Aishwarya Sivakumar <aishwaryasivakumar@Aishwaryas-MBP.lan>
2022-05-08 22:07:08 -0400

    Added lesson page

--- server.py
+++ server.py
@@ -57,25 +57,25 @@ learnInfo = {
 	},
 	"8": {
 			"id": "8",
-			"title": "Whole",
+			"title": "Whole Note",
 			"content": "But how do we know exactly how many beats to hold a note for? If you see a note without any vertical lines on it, then you hold it down for exactly 4 beats. This is also called a whole note.",
 			"image": ["https://study.com/cimages/multimages/16/773523164_whole_note.png"]
 	},
 	"9": {
 			"id": "9",
-			"title": "Half",
+			"title": "Half Note",
 			"content": "If you see a note that is white on the inside and has a vertical line attached to it, then you hold it down for exactly 2 beats. This is also called a half note.",
 			"image": ["https://media.istockphoto.com/vectors/black-music-half-note-on-staff-lines-vector-id1186530961?b=1&k=20&m=1186530961&s=170667a&w=0&h=3ly1SosFmcOheh8TtOPQmBqVmzbsyb4uol8XyFTT70w="]
 	},
 	"10": {
 			"id": "10",
-			"title": "Quarter",
+			"title": "Quarter Note",
 			"content": "If you see a note that is coloured black and has a vertical line attached to it, then you hold it down for exactly 1 beat. This is also called a quarter note.",
 			"image": ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC_YLoC6DhohF5wQNJVCayTLL2ok9obgp3j9Iqrd0T4wi9GyiK"]
 	},
 	"11": {
 			"id": "11",
-			"title": "Eighth",
+			"title": "Eighth Note",
 			"content": "And finally, if you see a note with a vertical line and a squiggly line on it, then you hold it down for exactly half a beat. This is also called an one eighth note.",
 			"image": ["https://upload.wikimedia.org/wikipedia/commons/2/23/Eighth_Note_1_%28PSF%29.png"]
 	},
@@ -253,6 +253,11 @@ right_ans = []
 @app.route('/')
 def welcome():
 	return render_template('welcome.html')
+
+
+@app.route('/lesson')
+def lesson():
+	return render_template('lesson.html')
 
 
 @app.route('/middle')
